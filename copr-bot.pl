b3985180c314e4458a94b1270f979cb2e237da6f
Athos Ribeiro <athoscribeiro@gmail.com>
2015-11-02 21:18:11 -0200

    Initial commit
    Signed-off-by: Athos Ribeiro <athoscribeiro@gmail.com>
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

---
+++ copr-bot.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use JSON;
+use LWP::Simple;
+use Data::Dumper;
+
+package MyBot;
+use base qw( Bot::BasicBot );
+my @urls = ("http://localhost:5000", "api", "status");
+#my $channel = "#softwarepublico";
+my $channel = "#ccsl-usp";
+my $json = JSON->new->allow_nonref;
+
+sub said {
+  #TODO: when someone asks for a package version, call her name.
+  my ($self, $message) = @_;
+  if ($message->{body} =~ /!version (.*)/) {
+    my $response = LWP::Simple::get(join("/", @urls));
+    my $info = $json->decode($response);
+    return $info->{$1}->{"git_version"};
+  }
+}
+
+sub tick {
+  my ($self) = @_;
+  my $response = LWP::Simple::get(join("/", @urls));
+  my $info = $json->decode($response);
+  my @mismatches;
+  foreach my $key (keys %{$info}) {
+    if($info->{$key} == 0) {
+      push(@mismatches, $key);
+    }
+  }
+
+  my $message = "Mismatched: ";
+  $message .= join(", ", @mismatches);
+
+  $self->say(channel => $channel, body=> $message) if @mismatches;
+  return 3600;
+}
+
+MyBot->new(
+  server => 'irc.freenode.net',
+  port => '6667',
+  channels => [ $channel ],
+  nick => 'spb_devops',
+)->run();
