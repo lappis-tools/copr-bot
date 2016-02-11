29b0477ce93099c21796d39e26236266d7e068e8
Athos Ribeiro <athoscribeiro@gmail.com>
2016-02-11 10:41:36 -0200

    add config file

--- copr-bot.pl
+++ copr-bot.pl
@@ -2,22 +2,25 @@
 use warnings;
 use strict;
 use JSON;
+use YAML::XS 'LoadFile';
 use LWP::Simple;
 use Data::Dumper;
 
+my $config = LoadFile('config.yaml');
+
 package MyBot;
 use base qw( Bot::BasicBot );
-my @urls = ("http://164.41.86.13/copr", "api", "status");
-my $channel = "#softwarepublico";
+my @urls = ($config->{BaseUrl}, "api", "status");
+my $channel = "#".$config->{Channel};
 my $json = JSON->new->allow_nonref;
 
 sub said {
   #TODO: when someone asks for a package version, call her name.
   my ($self, $message) = @_;
   if ($message->{body} =~ /!version (.*)/) {
-    my $response = LWP::Simple::get($urls[0].$urls[1]);
+    my $response = LWP::Simple::get($urls[0].'/'.$urls[1]);
     my $info = $json->decode($response);
-    return $info->{$1}->{"git_version"};
+    return $info->{$1}->{"git"}->{"master"};
   }
 }
 
@@ -36,7 +39,7 @@ sub tick {
   $message .= join(", ", @mismatches);
 
   $self->say(channel => $channel, body=> $message) if @mismatches;
-  return 3600;
+  return $config->{Tick};
 }
 
 MyBot->new(
